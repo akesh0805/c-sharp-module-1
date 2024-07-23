d29755948e944a5aca5e453e5a1e19744d7b1ef9
Developer <abdurakhmonovakbarkhon@gmail.com>
2024-07-23 20:19:17 +0500

    000

--- BigIntegers/Program.cs
+++ BigIntegers/Program.cs
@@ -1,23 +1,30 @@
-﻿using System.Numerics;
-// "Hello" - string literal
-// 123 - integer literal
-// 'a' - character literal
-// 3.14 - float literal
-BigInteger kattaSon = 123;
-Console.WriteLine(kattaSon);
+﻿// // using System.Numerics;
+// // // "Hello" - string literal
+// // // 123 - integer literal
+// // // 'a' - character literal
+// // // 3.14 - float literal
+// // BigInteger kattaSon = 123;
+// // Console.WriteLine(kattaSon);
 
-BigInteger kattaSon2 = BigInteger.Parse("20442342342342342342342342342342342346744073709551615");
-Console.WriteLine(kattaSon2);
+// // BigInteger kattaSon2 = BigInteger.Parse("20442342342342342342342342342342342346744073709551615");
+// // Console.WriteLine(kattaSon2);
 
-var kattaSon3 = new BigInteger(123);
+// // var kattaSon3 = new BigInteger(123);
 
-Console.WriteLine(kattaSon3.IsEven);
-Console.WriteLine(kattaSon3.IsOne);
-Console.WriteLine(kattaSon3.IsZero);
-Console.WriteLine(kattaSon3.IsPowerOfTwo);
-Console.WriteLine(BigInteger.Add(kattaSon3, kattaSon3));
-Console.WriteLine(BigInteger.Pow(kattaSon2, 1000));
+// // Console.WriteLine(kattaSon3.IsEven);
+// // Console.WriteLine(kattaSon3.IsOne);
+// // Console.WriteLine(kattaSon3.IsZero);
+// // Console.WriteLine(kattaSon3.IsPowerOfTwo);
+// // Console.WriteLine(BigInteger.Add(kattaSon3, kattaSon3));
+// // Console.WriteLine(BigInteger.Pow(kattaSon2, 1000));
 
-int a = int.MaxValue;
-a += 1;
-Console.WriteLine(a);
+// // int a = int.MaxValue;
+// // a += 1;
+// // Console.WriteLine(a);
+
+// float num = 3.12565899854f;
+// System.Console.WriteLine(num);
+// double abs = 5.65465131321324656546546541d;
+// System.Console.WriteLine(abs);
+// decimal asd = 3.55555555555555555555555555555555555555555555444444444444444444444444444444444444444444444444444444m;
+// System.Console.WriteLine(asd);
